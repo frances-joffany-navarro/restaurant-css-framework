02a5b43041a959c6c13ccba395c029d48127d367
frances <francesjoffanynavarro@gmail.com>
2024-03-02 17:48:53 +0100

    Revert "edit servername and password"
    This reverts commit 5eb910b5af3071192cae1794824584f107b539c7.

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "mysql";
+$servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "restaurant_dbase";
 
 //connect to database
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 
 //check connection; set the PDO error mode to exception mode
 $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
